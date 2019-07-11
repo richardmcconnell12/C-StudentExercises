0819ab881e63875bba747bd6e4060362c4a27b93
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-07-11 17:23:37 -0500

    Made part 1 a branch, cleaned up output in console

--- Student.cs
+++ Student.cs
@@ -26,6 +26,7 @@ namespace StudentExercise
         public void ViewAssignedExercises()
         {
             Console.WriteLine($"{FirstName} {LastName} list of exercises include: ");
+            Console.WriteLine("----------------------------");
             foreach (Exercise exercise in ExerciseList)
             {
                 Console.WriteLine($"{exercise.Name}: written in {exercise.Language}");
