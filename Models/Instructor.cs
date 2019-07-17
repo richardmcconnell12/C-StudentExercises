d4c2e0d3511f852c2e715f6fd4277496f382e1ff
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-07-17 11:10:06 -0500

    implemented inheritance to Instructors and Students

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 
 namespace StudentExercise
 {
-    class Instructor
+    class Instructor : NSSPerson
     {
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public string SlackHandle { get; set; }
-
         public string Specialty { get; set; }
-
-        public string _cohort;
 
         public Instructor(string firstname, string lastname, string slackhandle, string specialty, string cohort)
         {
---
+++ Parent Models/NSSPerson.cs
@@ -0,0 +1,15 @@
+// Similar properties
+// FirstName, LastName, SlackHandle, _cohort
+namespace StudentExercise
+{
+    public class NSSPerson
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string SlackHandle { get; set; }
+
+        public string _cohort { get; set; }
+    }
+}
